dd89a6cd41a21a22a818e0aac5c8adf8e04cd43b
cplatner <chris@platner.com>
2019-02-19 16:41:07 -0800

    - Added all 4 patterns

--- Haiku.cs
+++ Haiku.cs
@@ -15,7 +15,14 @@ public class Haiku
 	public static void Main()
 	{
 		var haiku = new Haiku(/*"words.txt"*/);
+		//* Write out all 4 patterns
 		haiku.Pattern1();
+		Console.WriteLine();
+		haiku.Pattern2();
+		Console.WriteLine();
+		haiku.Pattern3();
+		Console.WriteLine();
+		haiku.Pattern4();
 	}
 
 	public Haiku(/*string wordfile*/)
@@ -29,17 +36,80 @@ public class Haiku
 		Init();
 	}
 
+	/// <summary>
+	/// Print a Haiku with the following pattern:
+	///
+	/// Article Adjective Noun...
+	/// Article Noun Verb Preposition Article Noun
+	/// Adjective Adjective Noun
+	/// </summary>
 	public void Pattern1()
 	{
 		string adj = Adjective();
-		Console.WriteLine(string.Format("{0} {1} {2}...", GetArticle(adj), adj, Noun()));
+		Console.WriteLine($"{GetArticle(adj)} {adj} {Noun()}...");
 
 		string noun1 = Noun();
 		string noun2 = Noun();
-		Console.WriteLine(string.Format("{0} {1} {2} {3} {4} {5}",
-			GetArticle(noun1), noun1, Verb(), Preposition(), GetArticle(noun2), noun2));
+		Console.WriteLine($" {GetArticle(noun1)} {noun1} {Verb()} {Preposition()} {GetArticle(noun2)} {noun2}");
 
-		Console.WriteLine(string.Format("{0} {1} {2}", Adjective(), Adjective(), Noun()));
+		Console.WriteLine($"  {Adjective()} {Adjective()} {Noun()}");
+	}
+
+	/// <summary>
+	/// Print a Haiku with the following pattern:
+	///
+	/// Noun Preposition Article Noun;
+	/// Article Adjective Noun Preposition Article Noun
+	/// Adjective Noun
+	/// </summary>
+	public void Pattern2()
+	{
+		string noun1 = Noun();
+		Console.WriteLine($"{Noun()} {Preposition()} {GetArticle(noun1)} {noun1};");
+
+		string adj = Adjective();
+		string noun2 = Noun();
+		Console.WriteLine($" {GetArticle(adj)} {adj} {Noun()} {Preposition()} {GetArticle(noun2)} {noun2}");
+
+		Console.WriteLine($"  {Adjective()} {Noun()}");
+	}
+
+	/// <summary>
+	/// Print a Haiku with the following pattern:
+	///
+	/// Article Adjective Noun;
+	/// Preposition Article Adjective Noun
+	/// Article Noun Verb
+	/// </summary>
+	public void Pattern3()
+	{
+		string adj1 = Adjective();
+		Console.WriteLine($"{GetArticle(adj1)} {adj1} {Noun()};");
+
+		string adj2 = Adjective();
+		Console.WriteLine($" {Preposition()} {GetArticle(adj2)} {adj2} {Noun()}");
+
+		string noun2 = Noun();
+		Console.WriteLine($"  {GetArticle(noun2)} {noun2} {Verb()}");
+	}
+
+	/// <summary>
+	/// Print a Haiku with the following pattern:
+	///
+	/// Article Adjective Noun Verb;
+	/// Article Adjective Non
+	/// Preposition Article Adjective Noun
+	/// </summary>
+	public void Pattern4()
+	{
+		string adj1 = Adjective();
+		Console.WriteLine($"{GetArticle(adj1)} {adj1} {Noun()} {Verb()};");
+
+		string adj2 = Adjective();
+		Console.WriteLine($" {GetArticle(adj2)} {adj2} {Noun()}");
+
+		string adj3 = Adjective();
+		Console.WriteLine($"  {Preposition()} {GetArticle(adj3)} {adj3} {Noun()}");
 	}
 
 	private string GetArticle(string nextWord)
@@ -86,40 +156,34 @@ public class Haiku
 
     private void Init()
 	{
-		_articles = new List<string>();
-		_articles.AddRange(
-			new string[] { "a", "an", "the", "the" });
+		//* Note articles include an extra 'the' to make it more likely to appear
+		_articles = new List<string> { "a", "an", "the", "the" };
 
-		_adjectives = new List<string>();
-		_adjectives.AddRange(
-			new string[] { "autumn", "hidden", "bitter", "misty", "silent",
+		_adjectives = new List<string> {
+			"autumn", "hidden", "bitter", "misty", "silent",
 			"empty", "dry", "dark", "summer", "icy", "delicate", "quiet",
 			"white", "cool", "spring", "winter", "dappled", "twilight", "dawn",
 			"crimson", "wispy", "azure", "blue", "billowing", "broken", "cold",
 			"damp", "falling", "frosty", "green", "long", "late", "lingering",
 			"limpid", "still", "small", "sparkling", "throbbing", "vermilion",
-			"wandering", "withered", "wild", "black", "young" });
+			"wandering", "withered", "wild", "black", "young" };
 
-		_nouns = new List<string>();
-		_nouns.AddRange(
-			new string[] { "waterfall", "river", "breeze", "moon", "rain",
+		_nouns = new List<string> {
+			"waterfall", "river", "breeze", "moon", "rain",
 			"wind", "sea", "morning", "snow", "lake", "sunset", "pine",
 			"shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud",
 			"meadow", "sun", "glade", "bird", "brook", "butterfly", "bush",
 			"dew", "dust", "field", "fir", "flower", "firefly", "feather",
 			"grass", "haze", "mountain", "night", "pond", "shade", "snowflake",
 			"silence", "sound", "sky", "shape", "surf", "thunder", "violet",
-			"water", "wildflower", "wave" });
+			"water", "wildflower", "wave" };
 
-		_verbs = new List<string>();
-		_verbs.AddRange(
-			new string[] { "shakes", "drifts", "has stopped", "struggles",
+		_verbs = new List<string> {
+			"shakes", "drifts", "has stopped", "struggles",
 			"has fallen", "has passed", "sleeps", "creeps", "flutters",
-			"has risen", "is falling", "is trickling", "murmurs", "is floating" });
+			"has risen", "is falling", "is trickling", "murmurs", "is floating" };
 
-		_prepositions = new List<string>();
-		_prepositions.AddRange(
-			new string[] { "on", "of", "in", "under", "over", "near" });
+		_prepositions = new List<string> { "on", "of", "in", "under", "over", "near" };
 	}
 
 	private void Init_old(string wordfile)
